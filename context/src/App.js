cce8890e94e9aafb141f5c62733b3724271d83d3
Justin Trombley <jtrombley96@gmail.com>
2020-02-15 13:00:51 -0500

    Using Context Hook

--- context/src/App.js
+++ context/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// using context
+// create a ctx object - (createContext)
+// use the ctx obj "Provider" component to wrap the component tree
+// pass in the data we want to share to the component tree - this is done via a "value" prop that the Provider receives
+// consume that data in some child (nested) component
+
+const PersonContext = createContext();
+
+function App () {
+	const [
+		person,
+		setPerson,
+	] = useState({ name: 'Harry Potter' });
+
+	// const {Provider} = PersonContext
+	// Option 2^^^
+
+	return (
+		<div className='App'>
+			<PersonContext.Provider
+				value={[
+					person,
+					setPerson,
+				]}>
+				<Person otherProps={"I'm some different prop"} />
+			</PersonContext.Provider>
+
+			{/*<Provider>
+        <Person />
+    </Provider> 
+    ^^Option 2 */}
+		</div>
+	);
+}
+
+const Person = props => {
+	const [
+		person,
+		setPerson,
+	] = useContext(PersonContext); // consuming "person" from PersonContext!
+	// const {name} = useContext(PersonContext) This is a second option if not passing an array!
+	return (
+		<div>
+			<h1>{person.name}</h1>
+			<h2>{props.otherProps}</h2>
+			{/* <h1>{name}</h1> This can be used if not passing an array */}
+		</div>
+	);
+};
+
+// Render prop pattern to consume dcata from ctx without using the "useContext" hook
+
+const RenderPropPerson = () => {
+  
 }
 
 export default App;
